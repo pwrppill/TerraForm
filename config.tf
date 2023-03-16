8416852be8b30eda22cd2088b4265b70289658fb
pwrppill <pwrppill@gmail.com>
2023-03-16 17:21:44 +0300

    1.3.0

--- config.tf
+++ config.tf
@@ -47,14 +47,64 @@ mvn package
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-export AWS_ACCESS_KEY_ID=YCAJE0F3oZ1uGLB-9BqNNfdel
-export AWS_SECRET_ACCESS_KEY=YCMUOPIwSOgAtChRB5pB5FSwegS-tadYsGPcswUC
+export AWS_ACCESS_KEY_ID=*****
+export AWS_SECRET_ACCESS_KEY=*****
 aws --endpoint-url=https://storage.yandexcloud.net s3 cp target/hello-1.0.war s3://new-one/
 EOF
  }
 }
 
 resource "yandex_vpc_address" "addr" {
+  name = "external_ip"
+
+  external_ipv4_address {
+    zone_id = "ru-central1-b"
+  }
+}
+
+resource "yandex_compute_instance" "deploy" {
+  name = "deploy"
+  platform_id = "standard-v3"
+  zone = "ru-central1-b"
+
+  resources {
+    cores = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+   scheduling_policy {
+    preemptible = "true"
+  }
+
+  boot_disk {
+    initialize_params {
+        image_id = "fd8c3dv7t6prd7j4n162"
+        size = 8
+    }
+  }
+
+ network_interface {
+    subnet_id = "e2ls0jo68rhkjcrtkntj"
+    nat = "true"
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys = "cbrkd:${file("/home/cbrkd/.ssh/id_rsa.pub")}"
+#    user-data = "${file("/home/cbrkd/hw12/user-conf.yaml")}"
+    user-data = <<EOF
+#!/bin/bash
+sleep 300
+sudo apt update && sudo apt install tomcat9 awscli -y
+export AWS_ACCESS_KEY_ID=*****
+export AWS_SECRET_ACCESS_KEY=*****
+aws --endpoint-url=https://storage.yandexcloud.net s3 cp s3://new-one/hello-1.0.war /var/lib/tomcat9/webapps
+EOF
+ }
+}
+
+resource "yandex_vpc_address" "addr1" {
   name = "external_ip"
 
   external_ipv4_address {
